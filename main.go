7564213ccd4a5736a77dc71c6ac5cc9fd8d48bad
Dave Bakshani <dave@bakshani.net>
2017-06-16 23:48:30 -0700

    Removing commented out lines. Adjusting randomness of position & direction of moving circles.

--- main.go
+++ main.go
@@ -7,14 +7,12 @@ import (
 	"math"
 	"math/rand"
 	"runtime"
-	//"strconv"
 	"time"
 
 	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
 	"github.com/llgcode/draw2d"
 	"github.com/llgcode/draw2d/draw2dgl"
-	//"github.com/llgcode/draw2d/draw2dkit"
 )
 
 const (
@@ -23,10 +21,6 @@ const (
 
 var (
 	random *rand.Rand
-	// global rotation
-	//rotate int
-	//width, height int
-	//width, height = 1024, 1024
 	width, height = 512, 512
 	floatwidth, floatheight = float64(width), float64(height)
 	font draw2d.FontData
@@ -41,7 +35,6 @@ var circles [numCircles]circle
 
 // called when the window is reshaped/resized
 func reshape(window *glfw.Window, w, h int) {
-	//gl.ClearColor(1, 1, 1, 1)
 	gl.ClearColor(0, 0, 0, 1)
 	/* Establish viewing area to cover entire window. */
 	gl.Viewport(0, 0, int32(w), int32(h))
@@ -124,21 +117,24 @@ func onKey(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods 
 // initializes moving circles
 func initializeCircles() {
 	for i := 0; i < numCircles; i++ {
-		var direction int
-		if random.Intn(2) % 2 == 0 {
-			direction = 1 
-		} else {
-			direction = -1
-		}
 		c := circle{
-			xpos: random.Float64() * floatwidth,
-			ypos: random.Float64() * floatheight,
+			xpos: (floatwidth / 2) + (random.Float64() * float64(direction()) * floatwidth * 0.5),
+			ypos: (floatheight / 2) + (random.Float64() * float64(direction()) * floatheight * 0.5),
 			radius: random.Float64() * floatwidth * 0.03,
-			xdelta: random.Float64() * float64(direction) * floatwidth * 0.01,
-			ydelta: random.Float64() * float64(direction) * floatheight * 0.01,
+			xdelta: random.Float64() * float64(direction()) * floatwidth * 0.01,
+			ydelta: random.Float64() * float64(direction()) * floatheight * 0.01,
 			intensity: 255,
 		}
 		circles[i] = c
+	}
+}
+
+// returns a random +1 or -1 result; can be used for a random left/right or up/down direction
+func direction() int {
+	if random.Intn(2) % 2 == 0 {
+		return 1 
+	} else {
+		return -1
 	}
 }
 
@@ -182,7 +178,6 @@ func drawContents(w *glfw.Window) {
 		gl.Flush() /* single buffered, so needs a flush. */
 		time.Sleep(2 * time.Millisecond)
 		w.SwapBuffers()
-		//log.Println("angle: " + strconv.FormatFloat(i, 'f', -1, 32))
 		//time.Sleep(2 * time.Millisecond)
 	}
 }
@@ -208,8 +203,6 @@ func drawRadials(gc *draw2dgl.GraphicContext, x, y, width, height float64) {
 }
 
 func drawConcentricCircles(gc *draw2dgl.GraphicContext, xc, yc, width, height float64) {
-//	gc.Save()
-//	gc.Translate(x/2, y/2)
 	gc.SetLineWidth(2)
 	gc.SetStrokeColor(color.RGBA{0, 250, 0, 0xff})
 
@@ -221,18 +214,13 @@ func drawConcentricCircles(gc *draw2dgl.GraphicContext, xc, yc, width, height fl
 	}
 	xc = width/2
 	yc = height/2
-	//radiusX, radiusY := width/2, height/2
-	//radiusX, radiusY := width/2, height/2
-	//startAngle := 0 * (math.Pi / 180.0) /* angles are specified */
 	startAngle := 0.0
 	sweepAngle := 360 * (math.Pi / 180.0)     /* clockwise in radians           */
 //	gc.SetLineWidth(width / 10)
 	gc.SetLineCap(draw2d.ButtCap)
 //	gc.SetStrokeColor(image.Black)
 	for i := 1.0; i > 0; i = i - 0.3 { // reduction factor for concentric circles
-		//gc.MoveTo(xc + math.Cos(startAngle)*radiusX, yc + math.Sin(startAngle)*radiusY)
 		gc.MoveTo(xc + math.Cos(startAngle) * radius, yc + math.Sin(startAngle) * radius)
-		//gc.ArcTo(xc, yc, radiusX, radiusY, startAngle, sweepAngle)
 		gc.ArcTo(xc, yc, radius * i, radius * i, startAngle, sweepAngle)
 		gc.Stroke()
 	}
@@ -245,23 +233,13 @@ func drawSweep(gc *draw2dgl.GraphicContext, x, y, width, height float64, angle i
 	} else {
 		length = width/2
 	}
-	//for i := 0.0; i < 40; i = i + 0.5 {
 	for i := 0; i < 60; i++ {
 		gc.Save()
 		gc.Translate(x/2, y/2)
 		gc.SetLineWidth(1)
-		//gc.SetStrokeColor(color.RGBA{0, 250, 0, 0xff})
-		//gc.SetStrokeColor(color.RGBA{0, 250, 0, uint8(255 - i)})
 		gc.SetStrokeColor(color.RGBA{0, uint8(250 - 3 * i), 0, 0xff})
-		//gc.SetLineDash([]float64{3,1}, 0)
-		//gc.Rotate((angle - float64(float64(i) * 0.5)) * (math.Pi / 180.0))
 		gc.Rotate((float64(angle) - float64(i) * 0.5) * (math.Pi / 180.0))
 		gc.MoveTo(0, 0)
-//		if width > height {
-//			gc.LineTo(height * 0.5, 0)
-//		} else {
-//			gc.LineTo(width * 0.5, 0)
-//		}
 		gc.LineTo(length, 0)
 		gc.Stroke()
 		gc.Restore()
@@ -269,35 +247,15 @@ func drawSweep(gc *draw2dgl.GraphicContext, x, y, width, height float64, angle i
 }
 
 func drawMovingCircles(gc *draw2dgl.GraphicContext, xc, yc, width, height float64, intensity int) {
-//	gc.Save()
-//	gc.Translate(x/2, y/2)
 	gc.SetLineWidth(4)
-	//gc.SetStrokeColor(color.RGBA{0, 250, 0, 0xff})
 	gc.SetStrokeColor(color.RGBA{0, uint8(intensity), 0, 0xff})
-	//gc.SetFillColor(color.RGBA{0, 250, 0, 0xff})
-
-	//var radius float64
-	//if width > height {
-		//radius = height/2
-	//} else {
-		//radius = width/2
-	//}
-	//xc = width/2
-	//yc = height/2
-	//radiusX, radiusY := width/2, height/2
-	//radiusX, radiusY := width/2, height/2
-	//startAngle := 0 * (math.Pi / 180.0) /* angles are specified */
 	startAngle := 0.0
 	sweepAngle := 360 * (math.Pi / 180.0)     /* clockwise in radians           */
 //	gc.SetLineWidth(width / 10)
 	gc.SetLineCap(draw2d.ButtCap)
 //	gc.SetStrokeColor(image.Black)
-	//for i := 1.0; i > 0; i = i - 0.3 { // reduction factor for concentric circles
-		//gc.MoveTo(xc + math.Cos(startAngle)*radiusX, yc + math.Sin(startAngle)*radiusY)
-		gc.MoveTo(xc + math.Cos(startAngle) * width, yc + math.Sin(startAngle) * height)
-		//gc.ArcTo(xc, yc, radiusX, radiusY, startAngle, sweepAngle)
-		gc.ArcTo(xc, yc, width, height, startAngle, sweepAngle)
-		gc.Stroke()
-	//}
+	gc.MoveTo(xc + math.Cos(startAngle) * width, yc + math.Sin(startAngle) * height)
+	gc.ArcTo(xc, yc, width, height, startAngle, sweepAngle)
+	gc.Stroke()
 }
 
